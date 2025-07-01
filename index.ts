3e45f12e60cfe02a0a05d65471f4b17e9fc455dc
yewonneee <flower_d_o@naver.com>
2025-07-01 15:12:13 +0900

    first commit

---
+++ index.ts
@@ -0,0 +1,34 @@
+import { createAgent, IDIDManager } from '@veramo/core'
+import { KeyManager } from '@veramo/key-manager'
+import { KeyDIDProvider } from '@veramo/did-provider-key'
+import { DIDManager } from '@veramo/did-manager'
+import { MemoryPrivateKeyStore, KeyManagementSystem } from '@veramo/kms-local'
+import { MemoryDIDStore } from '@veramo/did-manager'
+import { MemoryKeyStore } from '@veramo/key-manager'
+
+const agent = createAgent<IDIDManager>({
+  plugins: [
+    new KeyManager({
+      store: new MemoryKeyStore(),
+      kms: {
+        local: new KeyManagementSystem(new MemoryPrivateKeyStore())
+      }
+    }),
+    new DIDManager({
+      store: new MemoryDIDStore(),
+      defaultProvider: 'did:key',
+      providers: {
+        'did:key': new KeyDIDProvider({
+          defaultKms: 'local'
+        })
+      }
+    })
+  ]
+})
+
+async function main() {
+  const identifier = await agent.didManagerCreate()
+  console.log('✅ 생성된 DID:', identifier.did)
+}
+
+main()
