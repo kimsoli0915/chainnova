d1b414fbdb0649d9e898685a666e96df072ae0f5
sunyoung <mintpotato888@gmail.com>
2025-07-01 17:39:25 +0900

    card 최종 수정

--- card/index.js
+++ card/index.js
@@ -1,8 +1,9 @@
 const { createAgent } = require('@veramo/core')
 const { KeyManager, MemoryKeyStore, MemoryPrivateKeyStore } = require('@veramo/key-manager')
-const { KeyDIDProvider } = require('@veramo/did-provider-key')
 const { DIDManager, MemoryDIDStore } = require('@veramo/did-manager')
+const { KeyDIDProvider } = require('@veramo/did-provider-key')
 const { KeyManagementSystem } = require('@veramo/kms-local')
+const { CredentialIssuer } = require('@veramo/credential-w3c')
 
 const agent = createAgent({
   plugins: [
@@ -20,13 +21,29 @@ const agent = createAgent({
           defaultKms: 'local'
         })
       }
-    })
+    }),
+    new CredentialIssuer()
   ]
 })
 
 async function main() {
   const identifier = await agent.didManagerCreate()
   console.log('✅ 생성된 DID:', identifier.did)
+
+  const verifiableCredential = await agent.createVerifiableCredential({
+    credential: {
+      issuer: { id: identifier.did },
+      issuanceDate: new Date().toISOString(),
+      credentialSubject: {
+        id: identifier.did,
+        name: '홍길동',
+        age: 30,
+      }
+    },
+    proofFormat: 'jwt',
+  })
+
+  console.log('✅ 발급된 VC:', verifiableCredential)
 }
 
 main()
