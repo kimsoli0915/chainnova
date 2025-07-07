0ec27529c0b7d0f00798af5bacd23cc59f4d0d5e
sunkk <so1io@naver.com>
2025-07-07 16:50:00 +0900

    feat: react.js 오류 수정

--- client/src/App.js
+++ client/src/App.js
@@ -1,24 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
+// src/App.js
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+
+// 4개 페이지 임포트 (대소문자 주의!)
+import PaymentInfo from './pages/payment/paymentinfo';
+import CardSelect from './pages/payment/cardselect';
+import CardInput from './pages/payment/cardinput';
+import PaymentResult from './pages/payment/paymentresult';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Routes>
+        <Route path="/" element={<PaymentInfo />} />
+        <Route path="/card-select" element={<CardSelect />} />
+        <Route path="/card-input" element={<CardInput />} />
+        <Route path="/result" element={<PaymentResult />} />
+      </Routes>
+    </Router>
   );
 }
 
