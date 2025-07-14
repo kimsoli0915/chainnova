0ecc22b865c9ea50acd7b35079db60f1b635d183
yewonneee <flower_d_o@naver.com>
2025-07-14 13:01:24 +0900

    ✨ VC 발급 + MetaMask 서명 + 로딩 UI 적용

--- client/src/App.js
+++ client/src/App.js
@@ -1,24 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+
+import PaymentIntro from './pages/payment/paymentIntro';
+import CardSelect from './pages/payment/cardselect';
+import CardInput from './pages/payment/cardinput';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Routes>
+        <Route path="/" element={<PaymentIntro />} />        {/* 1페이지: 결제 안내 */}
+        <Route path="/card-select" element={<CardSelect />} /> {/* 2페이지: 카드 선택 */}
+        <Route path="/card-input" element={<CardInput />} />   {/* 3페이지: 카드 정보 입력 */}
+      </Routes>
+    </Router>
   );
 }
 
